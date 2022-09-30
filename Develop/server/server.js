20b4fc281e498720da9b4cd92ea370799f7ebf3e
Brian Weinthal <76851117+189427@users.noreply.github.com>
2022-09-30 14:55:57 -0400

    cleaned code in server.js

--- Develop/server/server.js
+++ Develop/server/server.js
@@ -2,10 +2,14 @@ const express = require("express");
 const path = require("path");
 const db = require("./config/connection");
 const routes = require("./routes");
-const { ApolloServer } = require("apollo-server-express");
+const authMiddleware = require("./utils/auth");
+const ApolloServer = require("apollo-server-express");
+const { resolvers, typeDefs } = require("./schemas");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
